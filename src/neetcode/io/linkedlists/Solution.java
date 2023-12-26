afd999623185842f7caab3a064b5449f3e879368
ProgrammingPete <peter.parianos@yahoo.com>
2023-12-25 21:42:56 -0500

    test

--- src/neetcode/io/linkedlists/Solution.java
+++ src/neetcode/io/linkedlists/Solution.java
@@ -1,9 +1,15 @@
 package neetcode.io.linkedlists;
 
 public class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.test();
+    }
     void test(){
-        for(int i = 1, j = i -1; i < 100; i++){
-            // this is valid syntax
+        for(int i = 1, j = i - 1; i < 100; i++, j++){ // j is only set once at the beginning of hte list, and then it is incremented
+            // this is valid synta
+            System.out.println("i: " + i);
+            System.out.println("j: " + j);
         }
     }
 }
