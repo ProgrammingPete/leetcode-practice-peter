a79e4a403738e3439298ab12a529c6424d446722
ProgrammingPete <peter.parianos@yahoo.com>
2024-07-11 14:13:49 -0400

    update tests

--- src/neetcode/io/strings/WordCountEngineTest.java
+++ src/neetcode/io/strings/WordCountEngineTest.java
@@ -20,6 +20,23 @@ public class WordCountEngineTest {
             {"only", "1"}, 
             {"just", "1"}
         };
-        Assertions.assertTrue(Arrays.deepToString(expected).equals(Arrays.deepToString(sol.wordCountEngine(document))));
+        // Assertions.assertTrue(Arrays.deepToString(expected).equals(Arrays.deepToString(sol.wordCountEngine(document))));
+        Assertions.assertArrayEquals(expected, sol.wordCountEngine(document));
     }
+
+    @Test
+    void testWordCountEngine_test2(){
+        String document = "Practice makes perfect, you'll get perfecT by practice. just practice! just just just!!";
+        String[][] expected = new String[][] {
+            {"just", "4"},          
+            {"practice", "3"},
+            {"perfect", "2"},
+            {"makes", "1"},
+            {"youll", "1"},
+            {"get", "1"},
+            {"by", "1"}
+        };
+        Assertions.assertArrayEquals(expected, sol.wordCountEngine(document));
+    }
+
 }
